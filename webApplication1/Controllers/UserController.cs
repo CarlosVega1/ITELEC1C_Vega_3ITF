81fb93a77908f0fc16c534e2ec96b7e09d218d2b
Carlos Vega <carlosvincent.vega.cics@ust.edu.ph>
2023-09-06 23:02:23 +0800

    Add project files.

---
+++ webApplication1/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult MachineProblem()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
