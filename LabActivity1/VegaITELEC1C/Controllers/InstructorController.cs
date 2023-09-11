e3dc3daf2a323855ef20c566e08f9e8463ac480a
Carlos Vega <carlosvincent.vega.cics@ust.edu.ph>
2023-09-11 23:29:11 +0800

    ADDED FILES FOR LAB ACTIVITY

---
+++ LabActivity1/VegaITELEC1C/Controllers/InstructorController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VegaITELEC1C.Models;
+
+namespace VegaITELEC1C.Controllers
+{
+    public class InstructorController : Controller
+    {
+        List<Instructor> InstructorList = new List<Instructor>
+            {
+                new Instructor()
+                {
+                    Id= 1 , FirstName = "Gabriel", LastName = "Montano", isTenured = true, Rank = Rank.Instructor, HiringDate = DateTime.Parse("2022-08-26")
+                },
+                new Instructor()
+                {
+                    Id= 4 ,FirstName = "Shadow", LastName = "Garden", isTenured = true, Rank = Rank.Professor, HiringDate = DateTime.Parse("2022-1-25")
+                },
+                new Instructor()
+                {
+                    Id= 2 ,FirstName = "Alena", LastName = "Varona", isTenured = true , Rank = Rank.AssistantProfessor, HiringDate = DateTime.Parse("2022-08-07")
+                },
+                new Instructor()
+                {
+                    Id= 3 ,FirstName = "Carlos", LastName = "Vega", isTenured = false , Rank = Rank.AssociateProfessor, HiringDate = DateTime.Parse("2020-01-25")
+                }
+            };
+        public IActionResult Index()
+        {
+            
+            return View(InstructorList);
+        }
+
+        public IActionResult ShowDetail(int id)
+        {
+            
+            Instructor? instructor = InstructorList.FirstOrDefault(st => st.Id == id);
+
+            if (instructor != null)
+                return View(instructor);
+
+            return NotFound();
+        }
+
+    }
+
+}
+
---
+++ LabActivity1/VegaITELEC1C/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VegaITELEC1C.Models;
+
+namespace VegaITELEC1C.Controllers
+{
+    public class StudentController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            Student st = new Student();
+            st.StudentId = 1;
+            st.StudentName = "Carlos Vega";
+            st.DateEnrolled = DateTime.Parse( "30/8/2023");
+            st.Course = Course.BSIT;
+            st.Email = " carlosvincent.vega.cics@ust.edu.ph";
+            ViewBag.Student = st;
+            return View();
+        }
+    }
+}
